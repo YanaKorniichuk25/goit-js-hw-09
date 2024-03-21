981a2b244d66aa9b16862c146af6b81d15181f04
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2024-03-21 15:31:24 +0100

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,40 @@
-console.log('Form');
+const formEl = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+
+loadFromLS(formEl, storageKey);
+
+formEl.addEventListener('input', saveToLS);
+formEl.addEventListener('submit', handlerFormSubmit);
+
+function loadFromLS(form, key) {
+  const savedData = localStorage.getItem(key);
+
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    form.elements.email.value = email ?? '';
+    form.elements.message.value = message ?? '';
+  }
+}
+
+function saveToLS(e) {
+  const data = {
+    email: e.currentTarget.elements.email.value.trim(),
+    message: e.currentTarget.elements.message.value.trim(),
+  };
+
+  localStorage.setItem(storageKey, JSON.stringify(data));
+}
+function handlerFormSubmit(e) {
+  e.preventDefault();
+  const formData = JSON.parse(localStorage.getItem(storageKey)) || {};
+  const { email, message } = formData;
+
+  if (!email || !message) {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(storageKey);
+  formEl.reset();
+}
